936ea45817a85c35fa8a84138c0344b5a4429ede
Your Name <you@example.com>
2025-09-13 21:23:55 +0200

    Add last run test results JSON file with failure status

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,1 +1,12 @@
 // add any global test setup here
+import '@testing-library/jest-dom'
+
+globalThis.ResizeObserver = class ResizeObserver {
+  constructor(cb: any) {
+    this.cb = cb;
+  }
+  cb: any;
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+};
