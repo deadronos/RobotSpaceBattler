404fa0b53523439e7a5499f24a4c4922c65c2cde
deadronos <iridanblub@hotmail.de>
2025-10-22 07:35:52 +0200

    rewrite

--- src/components/battle/index.ts
+++
@@ -1,4 +0,0 @@
-export { BattleUI } from "./BattleUI";
-export { BattleUiHarness } from "./BattleUiHarness";
-export { CinematicHud } from "./CinematicHud";
-export { RobotOverlay } from "./RobotOverlay";
